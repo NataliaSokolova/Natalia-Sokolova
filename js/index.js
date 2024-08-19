1f7e800dfd9c3a0f396203ef43ef2990e1d73f84
Natalia Sokolova <nataliya.a.sokolova@gmail.com>
2024-08-19 16:38:57 -0700

    fixed index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 
-const newFooterEl = document.createElement('p');
-const textFooter = document.createTextNode('Phone: (425) 214-5050');
-newFooterEl.appendChild(textFooter);
-document.querySelector(".footer-section").appendChild(newFooterEl);
 
-
-
+const footer = document.createElement('footer');
+const footerContent = document.createElement('p');
+footer.appendChild(footerContent);
+const body = document.querySelector('body');
+body.appendChild(footer);
 const today = new Date(); 
 const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 copyright.innerHTML = `&copy; Sokolova ${thisYear}`;
-document.getElementById("connect").appendChild(copyright);
+footer.appendChild(copyright);
+
+
+
 
 const skills = ["JavaScript", "HTML", "CSS", "WordPress", "GitHub", "Figma", "SQL", "Digital Marketing"];
 const skillsSection = document.getElementById('skills');
